8251bce28575e2c1510054e88ff069ddb6fc0cc8
Yasser Tomawis <sertomawis@gmail.com>
2026-02-21 09:19:59 +0000

    Created grading logic

--- 4th Group Activity/4th.py
+++ 4th Group Activity/4th.py
@@ -5,3 +5,8 @@ def generate_papers(num_papers, num_questions):
         (random.randint(0, 1) for i in range(num_questions))
         for i in range(num_papers)
     }
+def grade_paper(paper):
+    total = 0
+    for answer in paper:
+        total += answer
+    return total
